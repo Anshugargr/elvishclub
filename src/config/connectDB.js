60c93fbf5cf84b6ebf07a36e712cf5bbc78a27f9
Anshuboss <anshugarg3672@gmail.com>
2024-04-13 21:12:25 +0530

    database change

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,9 +2,9 @@ import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
     host: 'sql.freedb.tech',
-    user: 'freedb_Aman7693',
-    password: '3w?X$&XJg3gP56b',
-    database: 'freedb_doublekamaii'
+    user: 'freedb_elvishclub',
+    password: 'puV4$K%5CwEaHxz',
+    database: 'freedb_elvishclub'
 });
 
 export default pool;
