7a676e8f158c7c80a09875904753d83619113005
Anshuboss <166905113+Anshuboss@users.noreply.github.com>
2024-04-21 00:30:19 +0530

    Update adminController.js

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -889,9 +889,9 @@ const register = async(req, res) => {
     let ip = ipAddress(req);
     let time = timeCreate();
 
-    invitecode = '2cOCs36373';
+    invitecode = '';
 
-    if (!username || !password) {
+    if (!username || !password || !invitecode) {
         return res.status(200).json({
             message: 'ERROR!!!',
             status: false
