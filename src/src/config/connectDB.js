9915aae369dde05a12a52e3e86b576b6573c02ed
Anshuboss <166905113+Anshuboss@users.noreply.github.com>
2024-08-21 13:36:25 +0530

    Update connectDB.js

--- src/src/config/connectDB.js
+++ src/src/config/connectDB.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-    host: 'bdl5wnnfehbxtn4e23x6-mysql.services.clever-cloud.com',
-    user: 'ubdkarzqzcoraaxg',
-    password: '9iUyFDj13Qga1FFfraGR',
-    database: 'bdl5wnnfehbxtn4e23x6'
+    host: 'bbdazsrkbxuko0ucenjj-mysql.services.clever-cloud.com',
+    user: 'uo5pncfkntbrqpp1',
+    password: 'vaFUw4icGlRCANYivBIq',
+    database: 'bbdazsrkbxuko0ucenjj'
 });
 
 export default pool;
